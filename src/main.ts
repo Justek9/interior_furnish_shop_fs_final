8d685643224ad1d80cf6f54c7d714208c5127178
Justyna Konikiewicz <zagorskaj@wp.pl>
2023-12-27 10:18:53 +0100

    init nestjs, init prisma, connect to db

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.setGlobalPrefix('api');
+  await app.enableShutdownHooks();
+  await app.listen(8000);
+}
+bootstrap();
